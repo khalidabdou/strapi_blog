2e65e415596bd450ffd835c1f977bf2ae3f6b084
abdellah khalid <37934938+khalidabdou@users.noreply.github.com>
2022-07-09 12:26:21 +0100

    strapi

--- config/server.js
+++ config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'http://strapi.khalidabdellah.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
-  cron: {
-    enabled: false,
-  }
+ 
 });
