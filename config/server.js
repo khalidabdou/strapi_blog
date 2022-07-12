2c034fe4cf92e498dab6154510c4f227dd122d44
abdellah khalid <37934938+khalidabdou@users.noreply.github.com>
2022-07-12 22:49:35 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://strapi.khalidabdellah.com',
+  url: 'https://strapi.khalidabdellah.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
