65e1bc68c4c9f6527665f566a6ceabb3f8ab056d
khalidabdou <37934938+khalidabdou@users.noreply.github.com>
2022-12-14 13:19:47 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -10,6 +10,7 @@ module.exports = ({ env }) => ({
       user: env('DATABASE_USERNAME', ''),
       password: env('DATABASE_PASSWORD', ''),
       ssl : false,
+      
       // ssl: {
       //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       // },
